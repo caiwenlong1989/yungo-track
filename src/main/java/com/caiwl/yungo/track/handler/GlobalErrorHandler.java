2ee22740e616cd8100db5bc60dd6711399507749
caiwl <caiwenlong@aloestec.com>
2019-01-16 15:07:46 +0800

    initial

--- src/main/java/com/caiwl/yungo/track/handler/GlobalErrorHandler.java
+++ src/main/java/com/caiwl/yungo/track/handler/GlobalErrorHandler.java
@@ -1,8 +1,18 @@
 package com.caiwl.yungo.track.handler;
 
+import org.rapidoid.http.Req;
+import org.rapidoid.http.Resp;
+import org.rapidoid.http.customize.ErrorHandler;
+import org.rapidoid.log.Log;
+
 /**
  * @author caiwl
  * @date 2019/1/16 11:09
  */
-public class GlobalErrorHandler {
+public class GlobalErrorHandler implements ErrorHandler {
+    @Override
+    public Object handleError(Req req, Resp resp, Throwable error) throws Exception {
+        Log.error("global error handler", error);
+        return null;
+    }
 }
